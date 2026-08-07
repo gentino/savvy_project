99675e22203b56c45fcab51fb5d03a1c5a914a7f
entec <miraobikz@gmail.com>
2026-08-07 20:42:08 +0100

    untracking files not needed

--- groups/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('groups',views.groups,name="groups"),
-    path('create',views.create,name="create_group"),
-    path('group/<int:id>',views.group,name="group")
-]
